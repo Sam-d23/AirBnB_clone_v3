fc49dff4201fbd428210d73b65c51ad729382fcd
Sam-d23 <samkiragu05@gmail.com>
2024-04-08 13:23:08 -0700

    Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""Creates Flask app"""
+"""
+A flask app is created here
+"""
 
-from os import getenv
-from flask import Flask
+from flask import make_response, jsonify, Flask
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_HOST', 5000))
-    app.run(host=HOST, port=PORT, threaded=True)
+@app.teardown_appcontext
+def storage_close():
+    """The storage is closed"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def err_404(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-'''
-New Flask app and app_views
-'''
+"""
+The blueprints app is in this file
+"""
 from api.v1.views import app_views
 from flask import jsonify
-
+from ..app import app
+from models.engine.db_storage import *
 
 @app_views.route('/status')
-def api_status():
-    """
+def status():
+    """The status of the api is returned"""
+    return jsonify({"status": "OK"})
 
-    """
-    response = {'status': "OK"}
-    return jsonify(response)
+@app.route('/api/v1/stats')
+def retrieve():
+    """The no. of items in classes is returned"""
+    new_dictionary = {}
+    for obj in classes:
+        total = count(obj)
+        new_dictionary[obj] = total
+    return jsonify(new_dictionary)
